963e2ef0d4857f92a19332cfd220113664c6ca76
Yoonsoon Chang <yoonsoon.chang@sada.com>
2021-06-30 15:58:09 -0400

    added os patch management

---
+++ modules/os-patch-management/data.tf
@@ -0,0 +1,10 @@
+data "google_client_config" "default" {
+  provider = google.tokengen
+}
+
+data "google_service_account_access_token" "sa" {
+  provider               = google.tokengen
+  target_service_account = var.service_account_impersonate
+  lifetime               = "3600s"
+  scopes                 = ["cloud-platform"]
+}
