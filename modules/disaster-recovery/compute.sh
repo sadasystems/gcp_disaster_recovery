3a4b2379f54136c0ad3ff5407100a21962b69e23
Yoonsoon Chang <yoonsoon.chang@sada.com>
2021-07-29 19:36:46 -0400

    bearer

--- modules/disaster-recovery/compute.sh
+++ modules/disaster-recovery/compute.sh
@@ -6,6 +6,8 @@ chmod 0755 jq
 
 eval "$(jq -r '@sh "SOURCE_VM=\(.source_vm) PROJECT=\(.project) ZONE=\(.zone)"')"
 
-CMD="gcloud compute instances describe $SOURCE_VM --project $PROJECT --zone $ZONE --format=json | jq ."
+#CMD="gcloud compute instances describe $SOURCE_VM --project $PROJECT --zone $ZONE --format=json | jq ."
+CMD="curl -H 'Authorization: Bearer ' -H 'Content-Type: application/json' 'https://compute.googleapis.com/compute/v1/projects/$PROJECT/zones/$ZONE/instances/$SOURCE_VM' | jq ."
+
 
 jq -n --arg vm "$(eval $CMD)" '{"source_vm":$vm}'
