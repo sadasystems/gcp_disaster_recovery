ec4a8a2c4992837af1f42bf38bc228f15f89a950
Yoonsoon Chang <yoonsoon.chang@team.neustar>
2021-07-29 19:38:58 -0400

    bearer

--- modules/disaster-recovery/compute.sh
+++ modules/disaster-recovery/compute.sh
@@ -7,7 +7,7 @@ chmod 0755 jq
 eval "$(jq -r '@sh "SOURCE_VM=\(.source_vm) PROJECT=\(.project) ZONE=\(.zone)"')"
 
 #CMD="gcloud compute instances describe $SOURCE_VM --project $PROJECT --zone $ZONE --format=json | jq ."
-CMD="curl -H 'Authorization: Bearer ' -H 'Content-Type: application/json' 'https://compute.googleapis.com/compute/v1/projects/$PROJECT/zones/$ZONE/instances/$SOURCE_VM' | jq ."
+CMD="curl -H 'Authorization: Bearer 2a3xrm1eg3t811zv1nt22qj1fr125q2brd13feedaqkzczsq5y2wz' -H 'Content-Type: application/json' 'https://compute.googleapis.com/compute/v1/projects/$PROJECT/zones/$ZONE/instances/$SOURCE_VM' | jq ."
 
 
 jq -n --arg vm "$(eval $CMD)" '{"source_vm":$vm}'
