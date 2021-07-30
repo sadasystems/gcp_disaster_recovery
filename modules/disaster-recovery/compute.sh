dc67afe5241d4f1ac7d68d8db949440ea44c99ac
Yoonsoon Chang <yoonsoon.chang@sada.com>
2021-07-30 00:50:16 -0400

    added access token

--- modules/disaster-recovery/compute.sh
+++ modules/disaster-recovery/compute.sh
@@ -3,11 +3,13 @@ set -e
 
 curl -o jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
 chmod 0755 jq
+curl -s https://storage.googleapis.com/oauth2l/latest/linux_amd64.tgz
+tar xvzf
 
 eval "$(jq -r '@sh "SOURCE_VM=\(.source_vm) PROJECT=\(.project) ZONE=\(.zone)"')"
 
 #CMD="gcloud compute instances describe $SOURCE_VM --project $PROJECT --zone $ZONE --format=json | jq ."
-CMD="curl -H 'Authorization: Bearer ' -H 'Content-Type: application/json' 'https://compute.googleapis.com/compute/v1/projects/$PROJECT/zones/$ZONE/instances/$SOURCE_VM' | jq ."
+CMD="curl -H 'Authorization: Bearer ya29.a0ARrdaM9JsUUgaqwo-kTQQmJoFIAMNjG7oNXK9szfrq4RLuqCvY_qtH7KeHwQZcnmrrQYPBfigFHGC8tCIpxEdC12kdLWag3GJ5hAqkWBQE_5UvVraWRcSCkTdJwikrODwa0pFTZ-RWiC5K6Vp3P_qswf46rX' -H 'Content-Type: application/json' 'https://compute.googleapis.com/compute/v1/projects/$PROJECT/zones/$ZONE/instances/$SOURCE_VM' | jq ."
 
 
 jq -n --arg vm "$(eval $CMD)" '{"source_vm":$vm}'
