b673e9af08d647937067733d78f6b0494941cce2
Yoonsoon Chang <yoonsoon.chang@sada.com>
2021-08-03 17:00:12 -0400

    added remote backend

--- modules/load-balancer/data.tf
+++ modules/load-balancer/data.tf
@@ -3,6 +3,17 @@ If you like to use Terraform workspace, please refer to the link
 https://www.terraform.io/docs/language/state/remote-state-data.html#example-usage-remote-backend-
 */
 data "terraform_remote_state" "backend" {
+  backend = "remote"
+
+  config {
+    organization = "hashicorp"
+    workspaces = {
+      name = "gcp_disaster_recovery"
+    }
+  }
+}
+/*
+data "terraform_remote_state" "backend" {
   backend = "local"
 
   config = {
@@ -10,3 +21,4 @@ data "terraform_remote_state" "backend" {
     path = "${path.module}/../unmanaged-instance-group-to-vm/terraform.tfstate"
   }
 }
+*/
