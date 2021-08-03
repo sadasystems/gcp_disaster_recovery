31610983b5fe73bbf66981140993321fd7261fb6
Yoonsoon Chang <yoonsoon.chang@sada.com>
2021-08-03 17:11:51 -0400

    added remote backend

--- modules/load-balancer/provider.tf
+++ modules/load-balancer/provider.tf
@@ -1,9 +1,9 @@
 provider "google" {
   version = "~> 3.0"
-  credentials = module.conjur.conjure_secret_value
+  credentials = module.conjur.conjur_secret_value
 }
 
 provider "google-beta" {
   version = "~> 3.0"
-  credentials = module.conjur.conjure_secret_value
+  credentials = module.conjur.conjur_secret_value
 }
