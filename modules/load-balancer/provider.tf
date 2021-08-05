c2ff33d9d2878abca038fd9c98b95a290944552b
Yoonsoon Chang <yoonsoon.chang@sada.com>
2021-08-05 13:12:20 -0400

    added env variable

--- modules/load-balancer/provider.tf
+++ modules/load-balancer/provider.tf
@@ -8,7 +8,4 @@ provider "google-beta" {
   credentials = module.conjur.conjur_secret_value
 }
 
-provider "tfe" {
-  hostname = "{$env.TFE_HOSTNAME}"
-  token = "{$env.TFE_TOKEN}"
-}
+provider "tfe" {}
