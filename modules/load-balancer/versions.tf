b97ad638a998d6637488d5c39bed300585172bde
Yoonsoon Chang <yoonsoon.chang@sada.com>
2021-08-06 13:33:21 -0400

    change provider

--- modules/load-balancer/versions.tf
+++ modules/load-balancer/versions.tf
@@ -4,14 +4,13 @@ terraform {
   required_providers {
     google = {
       version = ">= 3.0"
-      #source  = "hashicorp/google"
     }
     tfe = {
       version = "~> 0.25.0"
     }
-/*    conjur = {
+    conjur = {
       source  = "tfe.onedev.neustar.biz/OneDev/conjur"
       version = "1.0.0"
-    }*/
+    }
   }
 }
