8a4600c0ca7dc44480bfe5a069a1513dd0740537
Yoonsoon Chang <yoonsoon.chang@sada.com>
2021-08-06 13:02:06 -0400

    change provider

--- modules/load-balancer/provider.tf
+++ modules/load-balancer/provider.tf
@@ -7,8 +7,12 @@ provider "google-beta" {
   version = "~> 3.0"
   credentials = module.conjur.conjur_secret_value
 }
-/*
 
+provider "conjur" {
+  source = "tfe.onedev.neustar.biz/OneDev/conjur/google"
+  version = "1.0.0"
+}
+/*
 provider "tfe" {
   hostname = "tfe.onedev.neustar.biz"
   version = "0.13.5"
