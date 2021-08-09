89b7c2522de6e0cac958841fb235897ac21fb8ee
Yoonsoon Chang <yoonsoon.chang@sada.com>
2021-08-09 19:15:38 -0400

    switched to strategy

--- modules/disaster-recovery/provider.tf
+++ modules/disaster-recovery/provider.tf
@@ -1,9 +1,9 @@
 provider "google" {
-  version     = "~> 3.0"
+  version     = ">= 3.78.0"
   credentials = module.conjur.conjur_secret_value
 }
 
 provider "google-beta" {
-  version     = "~> 3.0"
+  version     = ">= 3.78.0"
   credentials = module.conjur.conjur_secret_value
 }
