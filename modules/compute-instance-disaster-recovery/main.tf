fa641c8f35abea8112a73c712de16f13918e51c4
Yoonsoon Chang <yoonsoon.chang@sada.com>
2021-08-12 23:24:40 -0400

    removed redendancy

--- modules/compute-instance-disaster-recovery/main.tf
+++ modules/compute-instance-disaster-recovery/main.tf
@@ -9,88 +9,21 @@ locals {
   autoscaler_name           = local.base_instance_name_prefix
 }
 
-resource "google_compute_disk" "default" {
-  count = length(var.disks)
+module "compute-instance" {
+  source = "../compute-instance"
 
-  project = var.project
-  name = var.disks[count.index].disk_name
-  type = var.disks[count.index].disk_type
-  size = var.disks[count.index].disk_size_gb
   zone = var.zone
-  image = var.disks[count.index].source_image
+  allow_stopping_for_update = false
+  disks = var.disks
   labels = var.labels
-}
-
-resource "google_compute_address" "internal_IP" {
-  name         = local.internal_ip_name
-  region       = var.region
-  project      = var.project
-  subnetwork   = local.subnetwork
-  address_type = "INTERNAL"
-}
-
-resource "google_compute_resource_policy" "hourly_backup" {
-  name    = local.snapshot_schedule_name
-  project = var.project
-  region  = var.region
-  snapshot_schedule_policy {
-    schedule {
-      hourly_schedule {
-        hours_in_cycle = var.snapshot.hours
-        start_time     = var.snapshot.start_time
-      }
-    }
-    retention_policy {
-      max_retention_days    = var.snapshot.max_retention_days
-      on_source_disk_delete = "KEEP_AUTO_SNAPSHOTS"
-    }
-  }
-}
-
-resource "google_compute_instance_template" "default" {
-  name_prefix         = local.instance_template_name
-
-  project      = var.project
-  region       = var.region
   machine_type = var.machine_type
-
-  tags = var.network_tag
-
-  metadata_startup_script = var.startup_script
-  labels = var.labels
   metadata = var.metadata
-
-  dynamic "disk" {
-    for_each = [for index, d in google_compute_disk.default: merge(d, var.disks[index])]
-    content {
-      source = disk.value["name"]
-      device_name = disk.value["device_name"]
-      resource_policies = [google_compute_resource_policy.hourly_backup.id]
-    }
-  }
-
-  network_interface {
-    subnetwork_project = var.subnetwork_project
-    subnetwork         = local.subnetwork
-    network_ip         = google_compute_address.internal_IP.address
-  }
-
-  scheduling {
-    on_host_maintenance = "MIGRATE"
-    automatic_restart   = true
-    preemptible         = false
-  }
-
-  service_account {
-    email = var.service_account.email
-    scopes = var.service_account.scopes
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-
-  depends_on = [google_compute_disk.default, google_compute_resource_policy.hourly_backup]
+  project = var.project
+  region = var.region
+  snapshot = var.snapshot
+  subnetwork = local.subnetwork
+  subnetwork_project = var.subnetwork_project
+  vm_name = var.vm_name
 }
 
 resource "google_compute_health_check" "http_autohealing" {
@@ -106,7 +39,7 @@ resource "google_compute_health_check" "http_autohealing" {
     port         = var.health_check["port"]
   }
 
-  depends_on = [google_compute_instance_template.default]
+  depends_on = [module.compute-instance.google_compute_instance_template.default]
 }
 
 resource "google_compute_health_check" "tcp_autohealing" {
@@ -122,7 +55,7 @@ resource "google_compute_health_check" "tcp_autohealing" {
     port = var.health_check["port"]
   }
 
-  depends_on = [google_compute_instance_template.default]
+  depends_on = [module.compute-instance.google_compute_instance_template.default]
 }
 
 resource "google_compute_autoscaler" "default" {
@@ -149,7 +82,7 @@ resource "google_compute_instance_group_manager" "mig" {
 
   version {
     name = local.instance_group_name
-    instance_template = google_compute_instance_template.default.id
+    instance_template = module.compute-instance.google_compute_instance_template.default.id
   }
 
   dynamic "named_port" {
@@ -166,7 +99,7 @@ resource "google_compute_instance_group_manager" "mig" {
   }
 
   dynamic "stateful_disk" {
-    for_each = google_compute_instance_template.default.disk
+    for_each = module.compute-instance.google_compute_instance_template.default.disk
     content {
       device_name = stateful_disk.value["device_name"]
     }
