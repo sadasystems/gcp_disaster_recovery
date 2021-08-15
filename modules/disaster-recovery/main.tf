f458edf8e9664441c11b55d0d8cb45235767f1ed
Yoonsoon Chang <yoonsoon.chang@sada.com>
2021-08-15 13:09:56 -0400

    disaster recovery update

--- modules/disaster-recovery/main.tf
+++ modules/disaster-recovery/main.tf
@@ -10,11 +10,8 @@ locals {
 
   subnetwork   = var.subnetwork == null ? data.google_compute_instance.source_vm.network_interface[0].subnetwork : var.subnetwork
   subnetwork_project =  var.subnetwork_project == null ? data.google_compute_instance.source_vm.network_interface[0].subnetwork_project : var.subnetwork_project
-  // : => =
-/*  source_disks = [for d in jsondecode(data.external.vm.result.source_vm).disks:
-  {for k, v in d: k => "${v}" }
-]*/
-  source_disks = [data.google_compute_instance.source_vm.boot_disk, data.google_compute_instance.source_vm.attached_disk]
+
+  source_disks = flatten(concat([data.google_compute_instance.source_vm.boot_disk], data.google_compute_instance.source_vm.attached_disk))
   disks = var.disks[0].disk_name == null ? local.source_disks : var.disks
   service_account = var.service_account == null ? jsondecode(data.external.vm.result.source_vm).serviceAccounts[0] : var.service_account
   images = var.disks[0].disk_name == null ? [for x in google_compute_image.images : { "source_image" = x.self_link }] : null
