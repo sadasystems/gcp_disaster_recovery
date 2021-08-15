789ac645a6914199bc21259801fbbd781e636357
Yoonsoon Chang <yoonsoon.chang@sada.com>
2021-08-15 15:11:38 -0400

    disaster recovery update

--- modules/disaster-recovery/main.tf
+++ modules/disaster-recovery/main.tf
@@ -31,13 +31,7 @@ locals {
   */
 
   source_disks = jsondecode(data.external.vm.result.source_vm).disks
-  temp_disks = [for d in local.source_disks:
-                [for x in d:{
-boot = x.boot
-auto_delete = x.autoDelete
-disk_name = x.deviceName
-}]
-  ]
+  temp_disks = [for i, d in local.source_disks: {for x in d: merge({ }, x)} ]
   disks = var.disks[0] == null ? local.temp_disks : var.disks
 
   service_account = var.service_account == null ? jsondecode(data.external.vm.result.source_vm).serviceAccounts[0] : var.service_account
