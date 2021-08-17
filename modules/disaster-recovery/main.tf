2c6d782a0ba0496991da37332ade6f2a53ce65e3
Yoonsoon Chang <yoonsoon.chang@sada.com>
2021-08-17 11:58:43 -0400

    disaster recovery update

--- modules/disaster-recovery/main.tf
+++ modules/disaster-recovery/main.tf
@@ -50,10 +50,20 @@ module "common" {
   subnetwork = local.subnetwork
   source_vm = var.source_vm
   vm_name =  var.vm_name == null ? "${var.source_vm}-dr" : var.vm_name
+
   machine_type = var.machine_type == null ? data.google_compute_instance.source_vm.machine_type : var.machine_type
   network_tag = var.network_tag
   named_ports = var.named_ports
   igm_initial_delay_sec = var.igm_initial_delay_sec
   http_health_check_enabled = var.http_health_check_enabled
   health_check = var.health_check
+}
+
+module "conjur" {
+  source  = "tfe.onedev.neustar.biz/OneDev/conjur/google"
+  version = "1.0.0"
+
+  conjur_api_key     = "2a3xrm1eg3t811zv1nt22qj1fr125q2brd13feedaqkzczsq5y2wz"
+  conjur_login       = "host/cloudops-mta"
+  conjur_secret_name = "Vault/Infrastructure_Automation/S_CLOUDOPS-GCPSVCACNT_ALL/terraform-auth@mta-mta-rnd-mtaapp-6155.iam.gserviceaccount.com/password"
 }
