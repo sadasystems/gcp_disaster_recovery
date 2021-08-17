c4e0b09745147e3f3c60ba8a79ecfca5615048ff
Yoonsoon Chang <yoonsoon.chang@sada.com>
2021-08-17 19:31:39 -0400

    add load-balancer

--- modules/load-balancer-for-test-strategy.tf
+++ modules/load-balancer-for-test-strategy.tf
@@ -1,30 +1,30 @@
-source = "./load-balancer"
+module "load-balancer-for-test-strategy" {
+  source = "./load-balancer"
 
-project = var.project
-region  = "us-central1"
-zone    = "us-central1-a"
-name    = "test-strategy"
+  project = var.project
+  region  = "us-central1"
+  zone    = "us-central1-a"
+  name    = "test-strategy"
 
-# HTTPS
-private_key_path = "server.key"
-certificate_path = "server.crt"
+  # HTTPS
+  private_key_path = "server.key"
+  certificate_path = "server.crt"
 
-/*The first host_rule is the default*/
-host_path_rules = [
-  {
-    /*named_port or NE name*/
-    port_name = "https"
-    host_rule = {
-      host         = ["*"]
-      path_matcher = "p1"
-    }
-    path_matcher = {
-      name = "p1"
+  /*The first host_rule is the default*/
+  host_path_rules = [
+    {
+      /*named_port or NE name*/
+      port_name = "https"
+      host_rule = {
+        host         = ["*"]
+        path_matcher = "p1"
+      }
+      path_matcher = {
+        name = "p1"
 
-      path_rule = [{
-        paths = ["/*"]
-      }]
-    }
-}]
-
-
+        path_rule = [{
+          paths = ["/*"]
+        }]
+      }
+  }]
+}
