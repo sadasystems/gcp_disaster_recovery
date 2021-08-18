79743adf4e84a7d9dd4baf65657d1ea56ae90f16
Yoonsoon Chang <yoonsoon.chang@sada.com>
2021-08-18 15:22:53 -0400

    add load-balancer

--- modules/load-balancer/data.tf
+++ modules/load-balancer/data.tf
@@ -3,16 +3,18 @@ If you like to use Terraform workspace, please refer to the link
 https://www.terraform.io/docs/language/state/remote-state-data.html#example-usage-remote-backend-
 */
 data "terraform_remote_state" "backend" {
-/*  backend = "remote"
+  backend = "remote"
 
   config = {
     organization = "MarketingSolutions-CA"
     workspaces = {
-      name = "gcp_disaster_recovery"
+      name = "{terraform.workspace}"
     }
-  }*/
+  }
+  /*
   backend = "local"
   config = {
     path = "${path.root}/terraform.tfstate"
   }
+  */
 }
